b00d5376719cbee283f9e520c95b8d1d7279ff7b
diegolopes87 <diegolopes087@gmail.com>
2020-08-17 15:33:42 -0300

    Tela principal

---
+++ src/IndexScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+const IndexScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Index</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default IndexScreen
